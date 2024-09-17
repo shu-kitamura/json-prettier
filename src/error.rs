8c44b5867b431f64adf769197a9356bc7e3b9503
shu-kitamura <shusei3316@yahoo.co.jp>
2024-09-17 10:55:41 +0900

    Parser Errorを追加

--- src/error.rs
+++ src/error.rs
@@ -4,12 +4,14 @@ use std::fmt::{self, Display};
 #[derive(Debug, PartialEq)]
 pub enum JsonPretError {
     LexerError(LexerError),
+    ParserError(ParserError),
 }
 
 impl Display for JsonPretError {
     fn fmt (&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            JsonPretError::LexerError(e) => write!(f, "LexerError: {}", e.message)
+            JsonPretError::LexerError(e) => write!(f, "LexerError: {}", e.message),
+            JsonPretError::ParserError(e) => write!(f, "ParserError: {}", e.message)
         }
     }
 }
@@ -30,18 +32,43 @@ impl LexerError {
     }
 }
 
+/// パース中に発生したエラー
+#[derive(Debug, PartialEq)]
+pub struct ParserError {
+    /// エラーメッセージ
+    pub message: String,
+}
+
+impl ParserError {
+    pub fn new(msg: &str) -> ParserError {
+        ParserError {
+            message: msg.to_string(),
+        }
+    }
+}
+
 
 // --- テストコード ---
 
 #[cfg(test)]
 mod tests {
-    use crate::error::LexerError;
+    use crate::error::*;
     #[test]
     fn test_lexer_error_new() {
-        let expect = LexerError {
+        let expect: LexerError = LexerError {
             message: "Error message".to_string()
         };
-        let actual = LexerError::new("Error message");
+        let actual: LexerError = LexerError::new("Error message");
+
+        assert_eq!(actual, expect);
+    }
+
+    #[test]
+    fn test_parser_error_new() {
+        let expect: ParserError = ParserError {
+            message: "Error message".to_string()
+        };
+        let actual: ParserError = ParserError::new("Error message");
 
         assert_eq!(actual, expect);
     }
