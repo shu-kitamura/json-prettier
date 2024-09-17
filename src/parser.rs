57e9b0aef182a85f235d72c033469dc721c6ea1a
shu-kitamura <shusei3316@yahoo.co.jp>
2024-09-17 11:52:34 +0900

    昼休みなので、一度コミット

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,6 @@
-use crate::lexer::Token;
+use std::collections::BTreeMap;
+
+use crate::{error::{JsonPretError, ParserError}, lexer::Token};
 
 pub struct Parser {
     /// `Lexer`で`tokenize`した`Token`一覧
@@ -10,6 +12,90 @@ pub struct Parser {
 impl Parser {
     pub fn new(tokens: Vec<Token>) -> Parser {
         Parser { tokens, index: 0 }
+    }
+
+    pub fn parse(&mut self) -> Result<(), JsonPretError>{
+        let peeked = self.peek();
+        if let Some(token) = peeked {
+            match token {
+                Token::LeftBrace => self.parse_object(),  // { の時の処理
+                Token::LeftBracket => self.parse_array(), // [ の時の処理
+                _ => Ok(())
+            }
+        } else {
+            Ok(())
+        }
+    }
+
+    fn parse_array(&mut self) -> Result<(), JsonPretError>{
+        todo!()
+    }
+
+    fn parse_object(&mut self) -> Result<(), JsonPretError>{
+        let token = match self.next_expect() {
+            Ok(t) => t.clone(),
+            Err(e) => return Err(e),
+        };
+
+        if token != Token::LeftBrace {
+            return Err(JsonPretError::ParserError(
+                ParserError::new(&format!("JSON object must start {{ but start {:?}", token))
+            ))
+        }
+
+        let mut obj: BTreeMap<String, String> = BTreeMap::new();
+
+        loop {
+            let t1: Token  = match self.next_expect() {
+                Ok(t) => t.clone(),
+                Err(e) => return Err(e)
+            };
+
+            if t1 == Token::RightBrace {
+                break;
+            }
+
+            let t2: Token  = match self.next_expect() {
+                Ok(t) => t.clone(),
+                Err(e) => return Err(e)
+            };
+
+            match (t1, t2) {
+                (Token::String(key), Token::Colon) => obj.insert(key, "test".to_string()),
+                _ => return Err(JsonPretError::ParserError(
+                    ParserError::new("a pair 'String(key)' and ':' is expected.")
+                ))
+            };
+        }
+
+        Ok(())
+    }
+
+    fn peek(&mut self) -> Option<&Token> {
+        self.tokens.get(self.index)
+    }
+
+    fn peek_expect(&mut self) -> Result<&Token, JsonPretError> {
+        match self.peek() {
+            Some(t) => Ok(t),
+            None => Err(JsonPretError::ParserError(
+                ParserError::new("a token isn't peekable")
+            ))
+        }
+    }
+
+    fn next(&mut self) -> Option<&Token> {
+        self.index += 1;
+        self.tokens.get(self.index-1)
+    }
+
+    fn next_expect(&mut self) -> Result<&Token, JsonPretError> {
+        match self.next() {
+            Some(t) => Ok(t),
+            None => Err(JsonPretError::ParserError(
+                ParserError::new("a token isn't peekable")
+            ))
+        }
     }
 }
 
